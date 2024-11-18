a22a05288db8a6722892e5e98b334b77d58325bc
Mahdi Hassani <m.hassani4951383@gmail.com>
2024-11-19 02:34:40 +0330

    login logic added

--- website/views.py
+++ website/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 def home(request):
+    # Check to see if logging in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You Have Been Logged In!")
+            return redirect('home')
+        
+        messages.error(request, "There Was An Error Logging in, Please Try Again...")
+        return redirect('home')
+            
     return render(request, 'home.html', {})
 
 def logout_user(request):
